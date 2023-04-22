fa8c7cad196de87e3164dc6a52d48e13fda4fc8b
rohitchauhan533 <chauhanrohit533@gmail.com>
2023-04-22 09:51:59 +0530

    added index

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,43 +1,50 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-const TodoList  = () => {
+const TodoList = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-    const [inputValue, setInputValue] = useState("");
-    const [tasks, setTasks] = useState([]);
-
-    const handleAddTask =()=>{
-        if(inputValue.trim()){
-            setTasks([...tasks, inputValue.trim()])
-            setInputValue("")
-        }
+  const handleAddTask = () => {
+    if (inputValue.trim()) {
+      setTasks([...tasks, inputValue.trim()]);
+      setInputValue("");
     }
+  };
 
-    const handleDeleteTask =(index)=>{
-       const  newTasks =[...tasks];
-       newTasks.splice(index,1);
-       setTasks(newTasks)
-        
-    }
+//   const handleDeleteTask = (index) => {
+//     const newTasks = [...tasks];
+//     newTasks.splice(index, 1);
+//     setTasks(newTasks);
+//   };
 
-    return (<div>
+const handleDeleteTask = (index) => () => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+  
 
-        <ul>
-            {tasks.map((task, index)=>(
-                
-                <li key="index">
-                {task}
-                <button onClick ={handleDeleteTask}>Delete</button>     
-                </li>
-            ))}
-        </ul>
-        <h1>my to-do list</h1>
-        <input  type ="text"
-            value = {inputValue}
-            onChange={(e)=>setInputValue(e.target.value)}
-        />
-        
-        <button onClick={handleAddTask}>add task</button>
-    </div>)
-}
+  return (
+    <div>
+      <ul>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
 
-export default TodoList
+      <button onClick={handleAddTask}>add task</button>
+        {tasks.map((task, index) => (
+          <li key="index">
+            {task}
+            <button onClick={handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <h1>my to-do list</h1>
+      
+    </div>
+  );
+};
+
+export default TodoList;
